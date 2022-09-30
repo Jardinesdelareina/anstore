33b0b3a2d14c9462f5ce3fef2e86a9eb3d333c0d
jardinesdelareina <jardinesdelareina@mail.ru>
2022-09-30 22:53:42 +0300

    cart.view

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from .serializers import *
 
-# Create your views here.
+
+class CartViewSet(ModelViewSet):
+    # Вывод корзины пользователя
+    serializer_class = CartSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'products_id'
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CartDetailSerializer
+        else:
+            return CartSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
