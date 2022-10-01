9b6f36f90d6d8476d36e12712e89d5a720e22247
jardinesdelareina <jardinesdelareina@mail.ru>
2022-10-01 09:51:33 +0300

    media

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'price', 'available', 'created_at', 'updated_at')
